0b45f6a0fde69c8c62c7b8a5f47667c30fed08f1
inforuser <inforuser@INHYDBEAN54.infor.com>
2017-05-29 14:59:02 +0530

    Sample-LogFileAdded

--- script/FirstProject.java
+++ script/FirstProject.java
@@ -9,6 +9,8 @@ public class FirstProject {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("TestTrial");
+		
 	}
 
 }
